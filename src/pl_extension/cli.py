54d838e7560179e246afb906c232dcfe81435853
min.du <min.du@horizon.ai>
2021-08-29 10:28:08 +0800

    init

---
+++ src/pl_extension/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+pl_extension.cli
+
+a simple command line to run pl_extension app.
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+def main():
+    print("hello, pl_extension")
+
+
+if __name__ == "__main__":
+    main()
