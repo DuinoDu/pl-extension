1111386a92e75047c9158056d6abe09b80b5ab15
min.du <min.du@horizon.ai>
2021-09-02 14:55:02 +0800

    add update_config

--- pl_extension/utilities/config.py
+++ pl_extension/utilities/config.py
@@ -2,8 +2,7 @@ import argparse
 import logging
 from typing import Any, Dict
 
-__all__ = ["add_options", "apply_options"]
-
+__all__ = ["add_options", "apply_options", "update_config"]
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +36,55 @@ def apply_options(config: Dict[str, Any], args: argparse.Namespace) -> None:
     if opts is not None and len(opts) > 0:
         config_CN.merge_from_list(opts)
     config.update(config_CN)
+
+
+def update_config(config: Dict[str, Any], vars_dict: Dict[str, Any]) -> None:
+    """Update str in config, with vars_dict.
+
+    When you want to set some lazy-compute value in config, `update_config` is
+    very useful.
+
+    Args:
+        config: input config dict.
+        vars_dict: key-value pair.
+
+    Examples:
+
+        config = dict()
+            a="python",
+            b="FILENAME",
+            c=["python", "FILENAME", "python FILENAME"],
+        )
+        vars_dict = {"FILENAME": "rust"}
+        update_config(config, vars_dict)
+        assert config["b"] == "rust"
+        assert config["c"][1] == "rust"
+        assert config["c"][2] == "python rust"
+
+    """
+
+    def _find_and_replace(_str):
+        for _k, _v in vars_dict.items():
+            _str = _str.replace(_k, _v)
+        return _str
+
+    if isinstance(config, dict):
+        for k in config:
+            if isinstance(config[k], str):
+                config[k] = _find_and_replace(config[k])
+            elif isinstance(config[k], dict):
+                update_config(config[k], vars_dict)
+            elif isinstance(config[k], list):
+                update_config(config[k], vars_dict)
+    elif isinstance(config, list):
+        for i, _ in enumerate(config):
+            if isinstance(config[i], str):
+                config[i] = _find_and_replace(config[i])
+            elif isinstance(config[i], dict):
+                update_config(config[i], vars_dict)
+            elif isinstance(config[i], list):
+                update_config(config[i], vars_dict)
+    elif isinstance(config, (int, float, bool)):
+        pass
+    else:
+        raise ValueError(f"Unknown type: {type(config)}")
